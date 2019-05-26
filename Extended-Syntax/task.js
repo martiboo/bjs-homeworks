fe4f18634b392afd1acfb4d752f6506ca71fb73b
martiboo <sanmarrino@gmail.com>
2019-05-26 13:11:39 +0300

    first commit Extended-Syntax

--- Extended-Syntax/task.js
+++ Extended-Syntax/task.js
@@ -1,5 +1,5 @@
 
-
+'use strict'
 function calculateQuadraticEquation(){
     let a = +window.a.value;
     let b = +window.b.value;
@@ -10,9 +10,24 @@ function calculateQuadraticEquation(){
     span.textContent = "х = "+result;
 }
 
-function getResult(a,b,c){
-    // код для задачи №1 писать здесь
-    //return x;
+function getResult(a,b,c) {
+    let d = Math.pow(b, 2) - 4 * a * c;
+    if (d < 0) {
+        console.log('Корней нет');
+    }
+    else if (d === 0) {
+        let x = - b / (2 * a);
+        console.log(x);
+        return x;
+    }
+    else if (d > 0) {
+        let x1 = (- b + Math.sqrt(d)) / (2 * a);
+        let x2 = (- b - Math.sqrt(d)) / (2 * a);
+        let arr = [x1, x2];
+        console.log(arr);
+        return arr;
+    }
+
 }
 
 function calculateDrinkTask(){
@@ -23,9 +38,16 @@ function calculateDrinkTask(){
 }
 
 function askDrink(name,dateOfBirthday){
-    // код для задачи №2 писать здесь
-    //console.log(result)
-    //return result;
+    let date = ( new Date ).getFullYear();
+    let age = date - dateOfBirthday.getFullYear();
+    if (age > 18) {
+        console.log(`Не желаете ли олд-фэшн, ${name} ?`);
+    }
+    else {
+        console.log(`Сожалею, ${name} ,но я не могу вам продать алкоголь. Зато могу предложить вам замечательный клюквенный компот!`);
+    }
+    console.log(age);
+    return age;
 }
 
 function calculateAverageRating(){
@@ -34,7 +56,20 @@ function calculateAverageRating(){
     window.averageMark.textContent = averageMark;
 }
 
-function getAverageMark(marks){
-    // код для задачи №3 писать здесь
-    //return averageMark;
+function getAverageMark(marks) {
+    let sum = 0;
+
+    if (marks.length > 5) {
+        let maxLength = marks.length - 5;
+        marks.splice(5,maxLength);
+        console.log('Считаются первые 5 оценок')
+    }
+
+    for (let i = 0; i < marks.length; i++) {
+        sum = sum + marks[i];
+    }
+
+    let averageMark = sum / marks.length;
+    console.log(averageMark);
+    return averageMark;
 }
