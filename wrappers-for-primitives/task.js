c93163045f17740d6e936be0387ae671c54418bc
martiboo <sanmarrino@gmail.com>
2019-05-31 21:05:16 +0300

    first commit wrappers-for-primitives

--- wrappers-for-primitives/task.js
+++ wrappers-for-primitives/task.js
@@ -1,3 +1,5 @@
+'use strict'
+
 function calculateMortgage() {
     let percent = window.percent.value;
     let contribution = window.contribution.value;
@@ -10,9 +12,42 @@ function calculateMortgage() {
 }
 
 function calculateTotalMortgage(percent, contribution, amount, date) {
+    if (isNaN(percent) === true) {
+        console.log(`Параметр Процентная ставка содержит неправильное значение ${percent}`);
+        percent = Number(percent);
+        if (isNaN(percent) === true) {
+            return;
+        }
+    }
 
-    // код для задачи №1 писать здесь
-    //return totalAmount;
+    if (isNaN(contribution) === true) {
+        console.log(`Параметр Первоначальный взнос содержит неправильное значение ${contribution}`);
+        percent = Number(contribution);
+        if (isNaN(contribution) === true) {
+            return;
+        }
+    }
+
+    if (isNaN(amount) === true) {
+        console.log(`Параметр Сумма кредита содержит неправильное значение ${amount}`);
+        amount = Number(amount);
+        if (isNaN(amount) === true) {
+            return;
+        }
+    }
+    
+    let dateTransform = new Date(date);
+    let debt = amount - contribution;
+    let s = debt;
+    let p = percent / 100;
+    let year = dateTransform.getFullYear() - new Date().getFullYear();
+    let month = dateTransform.getMonth() - new Date().getMonth();
+    let n = year * 12 + month;
+    let monthAmount = s*(p+p/(((1+p)^n)-1));
+    let totalAmount = monthAmount * n;
+    console.log(totalAmount);
+
+    return totalAmount;
 }
 
 function sayHello() {
@@ -23,6 +58,15 @@ function sayHello() {
 }
 
 function getGreeting(name) {
-    // код для задачи №2 писать здесь
-    //return greeting;
+    let greeting;
+    if (name === undefined || name === '' || name === null) {
+        greeting = 'Привет, мир! Меня зовут Аноним';
+        console.log(greeting);
+    }
+    else {
+        greeting = `Привет, мир! Меня зовут ${name}`;
+        console.log(greeting);
+    }
+    
+    return greeting;
 }
